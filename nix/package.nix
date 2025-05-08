6dc6c8e76d1235a704052c591d7a7257159a26a6
NotAShelf <raf@notashelf.dev>
2025-05-08 20:44:13 +0300

    nix: init tooling

---
+++ nix/package.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  rustPlatform,
+}: let
+  fs = lib.fileset;
+in
+  rustPlatform.buildRustPackage {
+    pname = "tempus";
+    version = "0.1.0";
+
+    src = fs.toSource {
+      root = ../.;
+      fileset = fs.unions [
+        (fs.fileFilter (file: builtins.any file.hasExt ["rs"]) ../src)
+        ../Cargo.lock
+        ../Cargo.toml
+      ];
+    };
+    cargoLock.lockFile = ../Cargo.lock;
+  }
